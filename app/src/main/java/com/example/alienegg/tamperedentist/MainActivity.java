982bb76a1d856a0ac9d12d819bb4b327e9415de6
Arbitor <alienlord209@gmail.com>
2016-04-02 18:23:53 +0300

    Minor stuff added, TODO comments.

--- app/src/main/java/com/example/alienegg/tamperedentist/MainActivity.java
+++ app/src/main/java/com/example/alienegg/tamperedentist/MainActivity.java
@@ -5,9 +5,34 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // URL = http://opendata.navici.com/tampere/opendata/ows?service=WFS&version=2.0.0&request=GetFeature&typeName=opendata:HAMMASHOITOLAT&outputFormat=json
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null)
+        {
+            // TODO Get data back or vice versa.
+        }
+
+        // TODO start fragment here.
+
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // TODO On Resume
+    }
+
+    @Override
+    public void onDestroy ()
+    {
+        super.onDestroy();
+        // TODO On Destroy.
+    }
+
 }
